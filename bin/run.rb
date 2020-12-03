dc82eca391c89516fcaffbe583546dd82743319d
jeremy-rodrguez <jeremy.rodrguez@yahoo.com>
2020-12-03 15:47:42 -0800

    Created CLI file in lib

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,12 @@
 require_relative '../config/environment'
 
-puts "hello world"
+# puts "hello world"
+
+# def greet
+#     puts "Welcome to SKOOL, the single source of truth for all klass reviews!"
+# end
+
+# greet
+
+cli = CommandLineInterface.new
+cli.greet 
---
+++ lib/cli.rb
@@ -0,0 +1,5 @@
+def greet
+    puts "Welcome to SKOOL, the single source of truth for all klass reviews!"
+end
+
+greet
